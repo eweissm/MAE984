aabc1ef5fdc1bfae5bc37e620c654151ea87e67b
Ericw <eweissm1@asu.edu>
2022-08-23 21:14:41 -0700

    hw1 v1

---
+++ HW1/main.py
@@ -0,0 +1,14 @@
+from scipy.optimize import minimize
+import numpy as np
+x0 = np.array(1., 1., 1., 1., 1.)
+
+eqn = lambda x: (x[0] - x[1]) ** 2 + (x[1] + x[2] - 2) ** 2 + (x[3] - 1) ** 2 + (x[4] - 1) ** 2
+bnds = ((-10., 10.), (-10., 10.), (-10., 10.), (-10., 10.), (-10., 10.))
+
+cnst = dict({'type': 'eq', 'fun': lambda x: x[0] + 3. * x[1]},
+            {'type': 'eq', 'fun': lambda x: x[2] + x[3] - 2. * x[4]},
+            {'type': 'eq', 'fun': lambda x: x[1] - x[4]})
+
+res = minimize(eqn, x0, method='SLSQP', bounds=bnds, constraints=cnst)
+
+print(res)
