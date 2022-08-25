9d1ddce56d23c41d8e3b1a8011f2896fee6f445d
Ericw <eweissm1@asu.edu>
2022-08-25 10:54:38 -0700

    python hw 1 v1

--- pythonHW1/main.py
+++ pythonHW1/main.py
@@ -1,4 +1,3 @@
-
 from scipy.optimize import minimize
 import numpy as np
 x0 = np.array(1., 1., 1., 1., 1.)
@@ -7,8 +6,8 @@ eqn = lambda x: (x[0] - x[1]) ** 2 + (x[1] + x[2] - 2) ** 2 + (x[3] - 1) ** 2 + 
 bnds = ((-10., 10.), (-10., 10.), (-10., 10.), (-10., 10.), (-10., 10.))
 
 cnst = [{'type': 'eq', 'fun': lambda x: (x[0] + 3. * x[1])},
-        {'type': 'eq', 'fun': lambda x: x[2] + x[3] - 2. * x[4]},
-        {'type': 'eq', 'fun': lambda x: x[1] - x[4]}]
+       {'type': 'eq', 'fun': lambda x: x[2] + x[3] - 2. * x[4]},
+       {'type': 'eq', 'fun': lambda x: x[1] - x[4]}]
 
 res = minimize(eqn, x0, method='SLSQP', bounds=bnds, constraints=cnst)
 
